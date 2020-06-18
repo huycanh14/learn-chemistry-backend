72ac30405fa6d24c39829cbc2915a4b61fd8fdc1
Kain <36950884+huycanh14@users.noreply.github.com>
2020-06-18 14:41:57 +0700

    Update app.js
    edit cros

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 //add cros
-app.use(cors());
+app.use(cors({origin: true, credentials: true}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
