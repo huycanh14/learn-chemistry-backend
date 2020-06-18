867114d6430159d63a516e5cfe245f73b827fb1d
Kain <36950884+huycanh14@users.noreply.github.com>
2020-06-18 14:47:05 +0700

    Update app.js
    add res header

--- app.js
+++ app.js
@@ -42,6 +42,12 @@ app.set('view engine', 'jade');
 
 //add cros
 app.use(cors({origin: true, credentials: true}));
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
