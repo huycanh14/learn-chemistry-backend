a94fc0516cc1e1e7e6535478cf576fac070c313a
Huy Canh <huycanh14@gmail.com>
2020-06-19 18:19:01 +0700

    add package cros

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// var cors = require('cors'); // Cross-Origin Resource Sharing
+var cors = require('cors'); // Cross-Origin Resource Sharing
 // declare mongoose
 var mongoose = require('mongoose');
 
@@ -41,20 +41,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 //add cros
-// app.use(cors());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// edit cros to use api
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 app.use(function (req,res,next) { req.url = req.url.replace(/[\/]+/g, '/'); next(); });
 
 // use TokenCheckMiddleware
