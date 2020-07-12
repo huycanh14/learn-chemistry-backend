0bd24bf54dfa967ec36ec67607008f1c9350a34a
Huy Canh <huycanh14@gmail.com>
2020-07-12 10:16:38 +0700

    change time refreshTokenLife

--- helpers/config.js
+++ helpers/config.js
@@ -3,7 +3,7 @@ var config = {
     refreshTokenSecret: "some-s3cret-refre2h-token",
     port: 3000,
     tokenLife: 7200, // 2h: 
-    refreshTokenLife: 86400 // một ngày
+    refreshTokenLife: 604800 // một tuần
 }
 
 module.exports = config;
